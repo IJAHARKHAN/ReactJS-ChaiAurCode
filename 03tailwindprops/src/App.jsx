8c658bd897175a7771c0530469f0708c5391b6cd
IJAHARKHAN <khan7oct@gmail.com>
2023-08-30 23:16:46 +0530

    Update HTMKL Content

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { React, useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <>
-      <h1 className='bg-green-400 text-center p-4 rounded-xl'>Tailwind Test</h1>
-      
+      <h1 className="bg-green-400 text-center p-4 rounded-xl">Tailwind Test</h1>
+      <Card channel='MoonFrontend'  btnText='Click me'/>
+      <Card channel='IjaharFrontend'/>
+      {/* <Card makeobject={myObj} /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
